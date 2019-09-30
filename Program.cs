adf15e6f205a138455ca564aa1d78a860b6b827a
Haffar Yassine <Haffar@onee.ma>
2019-09-30 16:48:54 +0100

    Commentaire modification 1

--- Program.cs
+++ Program.cs
@@ -19,6 +19,6 @@ namespace Monster
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>();//naruto
     }
 }
